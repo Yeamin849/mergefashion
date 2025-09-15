dac6ae47ca94eac83540a6bb7ff51aff4db9dcd6
Yeamin <yeaminh849@gmail.com>
2025-09-15 16:34:16 +0600

    Add Bengali instructions for ordering and calling on product page

--- resources/views/public/product.blade.php
+++ resources/views/public/product.blade.php
@@ -204,12 +204,14 @@
                                                 class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
                                                 Call Now
                                             </a>
+                                            <p class="mt-2">সরাসরি ওয়েবসাইটে অর্ডার করতে Buy Now বাটনে ক্লিক করুন । </p>
+                                            <p class="mt-2">যে কোন তথ্য জানতে Call Now বাটনে ক্লিক করুন । </p>
                                         </div>
                                     </div>
                                 </form>
 
                         </section>
-                        <h2 class="mb-2 text-xl font-semibold leading-none text-gray-900 md:text-2xl dark:text-white">
+                        {{-- <h2 class="mb-2 text-xl font-semibold leading-none text-gray-900 md:text-2xl dark:text-white">
                             Foot size for shoes
                         </h2>
 
@@ -230,7 +232,7 @@
                                     </svg>
                                 </div>
                             </div>
-                        </a>
+                        </a> --}}
 
                         <!-- Modal Structure -->
                         <div id="imageModal"
